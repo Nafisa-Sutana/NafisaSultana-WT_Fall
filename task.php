441d1db760c61fd6e8641fdd0786dd2fcadf9b87
Nafisa Sultana <90836770+Nafisa-Sutana@users.noreply.github.com>
2022-10-17 23:31:54 +0600

    Add files via upload

---
+++ task.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title>Registration form for seller</title>
+</head>
+<body>
+	<form>
+		<table>
+			<tr>
+				<td><First name></td>
+					<td><input type="text" placeholder="Please Enter your first name" name="name"></td>
+				</tr>
+				<tr>
+				<td><Last name></td>
+					<td><input type="text" placeholder="Please Enter your last name" name="name"></td>
+				</tr>
+
+				<tr>
+				<td><Age></td>
+					<td><input type="number" name="age"></td>
+				</tr>
+
+				<tr>
+				<td><Email Address></td>
+					<td><input type="text"  name="email"></td>
+				</tr>
+
+				<tr>
+				<td><Gender></td>
+					
+				</tr>
+				<tr>
+					<td>Male<input type="radio" name="Gender"></td>
+					<td>Female<input type="radio" name="Gender"></td>
+					<td>Others<input type="radio" name="Gender"></td>
+				</tr>
+
+
+				<tr>
+				<td><input type="sumbit"  name="sumbit"></td>
+					
+				</tr>
+			</form>
+
+
+
+
+
+				
+
+
+
+</body>
+</html>

2305965ba60c5be13b0f4c30e88a7fef221be71d
Nafisa Sultana <90836770+Nafisa-Sutana@users.noreply.github.com>
2022-11-07 00:19:15 +0600

    Update task.php

--- task.php
+++ task.php
@@ -1,53 +1,63 @@
+<!DOCTYPE html>
+
 <html>
+
 <head>
-	<title>Registration form for seller</title>
+
+    <title>Registration Form for Seller</title>
+
 </head>
+
 <body>
-	<form>
-		<table>
-			<tr>
-				<td><First name></td>
-					<td><input type="text" placeholder="Please Enter your first name" name="name"></td>
-				</tr>
-				<tr>
-				<td><Last name></td>
-					<td><input type="text" placeholder="Please Enter your last name" name="name"></td>
-				</tr>
+<form>
+		
+        
 
-				<tr>
-				<td><Age></td>
-					<td><input type="number" name="age"></td>
-				</tr>
+        <h2>Registration form</h2>
 
-				<tr>
-				<td><Email Address></td>
-					<td><input type="text"  name="email"></td>
-				</tr>
+        
 
-				<tr>
-				<td><Gender></td>
-					
-				</tr>
-				<tr>
-					<td>Male<input type="radio" name="Gender"></td>
-					<td>Female<input type="radio" name="Gender"></td>
-					<td>Others<input type="radio" name="Gender"></td>
-				</tr>
+        <label>Your First Name</label>
+
+        <input type="text" name="Fname" placeholder="Please enter your first Name"><br>
 
 
-				<tr>
-				<td><input type="sumbit"  name="sumbit"></td>
-					
-				</tr>
-			</form>
+        <label>Your Last Name</label>
+        <input type="text" name="Lname" placeholder="Please enter your last Name"><br>
+        
+
+        <label> Date of birth </label>
+
+        <input type="number" name="dob" placeholder="Please enter Dob"><br>
+        
+
+        <label>Current Address</label>
+
+        <input type="text" name="address" placeholder="Please enter your current address"><br>
+        
+        <label> Gender</label>
+        <td>Male<input type="radio" name="Gender"></td>
+	    <td>Female<input type="radio" name="Gender"></td>
+		<td>Others<input type="radio" name="Gender"></td>
+
+        <br>
 
 
+    
 
+        <label>Password</label>
 
+        <input type="password" name="password" placeholder="Please Enter Your Password"><br>
+        
+        <label>Confirm Password</label>
 
-				
+        <input type="password" name="password" placeholder="Please  Confirm Your Password"><br>
 
+        <button type="Register">Registration</button>
 
+     </form>
 
 </body>
+
 </html>
+
